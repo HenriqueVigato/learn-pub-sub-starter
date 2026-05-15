f0c7c72b51cc89e58ada728564c6bc3d02aee788
Henrique <henriquevigato@gmail.com>
2026-05-15 17:19:11 -0300

    Finalizando a L-4 CH-2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	rabbitURL := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(rabbitURL)
+	if err != nil {
+		fmt.Errorf("erro ao estabelecer conecao com o rabbitmq %v", err)
+	}
+	defer connection.Close()
+
+	fmt.Println("RabbitMq connection success")
+	fmt.Println("Starting server...")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Printf("\nClosing RabbitMQ connection ...\n")
+	connection.Close()
+	fmt.Println("Closed")
 }
