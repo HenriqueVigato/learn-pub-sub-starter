9dd65a029a2a43e885f2caf7a7e959c1cbfc21de
Henrique <henriquevigato@gmail.com>
2026-05-16 11:29:25 -0300

    Finalizando a L-1 CH-3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,22 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	fmt.Println("Starting Peril server...")
+
 	rabbitURL := "amqp://guest:guest@localhost:5672/"
 	connection, err := amqp.Dial(rabbitURL)
 	if err != nil {
-		fmt.Errorf("erro ao estabelecer conecao com o rabbitmq %v", err)
+		log.Fatalf("failed to connect to rabbitmq %v", err)
 	}
 	defer connection.Close()
 
 	fmt.Println("RabbitMq connection success")
-	fmt.Println("Starting server...")
+
+	ch, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("failed to create a chanel err: %v", err)
+	}
+
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("Error can't publis the data %v", err)
+	}
 
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
