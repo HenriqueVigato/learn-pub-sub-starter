29b95101fe717d7ba6fc74ccd6b09f2d02bbd070
Henrique <henriquevigato@gmail.com>
2026-05-16 12:23:21 -0300

    Finalizando a L-5 CH-3

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	rabbitURL := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(rabbitURL)
+	if err != nil {
+		log.Fatalf("failed to connect to rabbitmq %v", err)
+	}
+	defer connection.Close()
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Couldn't get user name %v", err)
+	}
+
+	chanel, _, err := pubsub.DeclareAndBind(connection, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, username), routing.PauseKey, pubsub.Transient)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Printf("\nClosing connection ...\n")
+	connection.Close()
+	chanel.Close()
+	fmt.Println("Closed")
 }
---
+++ internal/pubsub/queue_handler.go
@@ -0,0 +1,29 @@
+package pubsub
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+type SimpleQueueType int
+
+const (
+	Durable SimpleQueueType = iota
+	Transient
+)
+
+func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return ch, amqp.Queue{}, err
+	}
+
+	queue, err := ch.QueueDeclare(queueName, queueType == Durable, queueType == Transient, queueType == Transient, false, nil)
+	if err != nil {
+		return ch, amqp.Queue{}, err
+	}
+
+	err = ch.QueueBind(queue.Name, key, exchange, false, nil)
+	if err != nil {
+		return ch, amqp.Queue{}, err
+	}
+
+	return ch, queue, nil
+}
