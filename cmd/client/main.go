c800c4cf9451224471d39534cad78f71053cbeba
Henrique <henriquevigato@gmail.com>
2026-05-18 16:40:16 -0300

    Finalizando L-7 CH-3

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -27,17 +25,48 @@ func main() {
 		log.Fatalf("Couldn't get user name %v", err)
 	}
 
-	chanel, _, err := pubsub.DeclareAndBind(connection, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, username), routing.PauseKey, pubsub.Transient)
+	channel, _, err := pubsub.DeclareAndBind(connection, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, username), routing.PauseKey, pubsub.Transient)
 	if err != nil {
 		log.Fatalf("couldn't make a chanel %v", err)
 	}
+	defer channel.Close()
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	gameState := gamelogic.NewGameState(username)
+	for {
+		userInput := gamelogic.GetInput()
+		if len(userInput) == 0 {
+			continue
+		}
 
-	fmt.Printf("\nClosing connection ...\n")
-	connection.Close()
-	chanel.Close()
-	fmt.Println("Closed")
+		switch userInput[0] {
+		case "spawn":
+			err := gameState.CommandSpawn(userInput)
+			if err != nil {
+				fmt.Printf("\n Erro: %v\n", err)
+				continue
+			}
+		case "move":
+			if len(userInput) < 3 {
+				fmt.Println("the move command should have 2 options")
+				continue
+			}
+			_, err := gameState.CommandMove(userInput)
+			if err != nil {
+				fmt.Printf("\n Erro: %v\n", err)
+				continue
+			}
+			fmt.Println("Move succes")
+		case "status":
+			gameState.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "quit":
+			gamelogic.PrintQuit()
+			return
+		default:
+			fmt.Println("Commando invalide")
+		}
+	}
 }
